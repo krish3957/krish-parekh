3e4d9464df0a0d90ed97d8fc4aafce1d48bb1701
krish3957 <kpparekh81602@gmail.com>
2024-07-09 16:41:27 +0530

    Update project images and descriptions

--- public/data.ts
+++ public/data.ts
@@ -3,35 +3,49 @@ const data = [
         'id': 1,
         'name': ' E-Commerce',
         'description': 'Crafting Seamless Shopping: E-Commerce Haven merges technology and retail, offering a user-centric e-commerce experience. My role as a full-stack developer involved implementing secure login, domain integration, smooth cart management, and PhonePe payment integration for a streamlined shopping journey.',
-        'img':require('./E-com.jpg'),
+        'img': require('./E-com.jpg'),
         'link': 'https://Sev7n.in',
     },
     {
         'id': 2,
         'name': 'Lost & Found Explorer',
         'description': 'Lost&Found Explorer goes beyond a platform, fostering connections within the campus community. I developed this solution to empower users to report and discover lost items, creating a sense of unity through intuitive features.',
-        'img':require('./lost&found.jpg'),
+        'img': require('./lost&found.jpg'),
         'link': 'https://github.com/krish3957/lostandfound-website',
     },
     {
         'id': 3,
         'name': 'Task Mastermind',
         'description': 'Task Mastermind, a task management web app, elevates productivity with precision. As a developer, I designed a platform allowing users to effortlessly add tasks with deadlines and priorities, providing a comprehensive view of pending, completed, and ongoing tasks.',
-        'img':require('./task-mastermind.jpg'),
+        'img': require('./task-mastermind.jpg'),
         'link': 'https://github.com/krish3957/task-management-frontend',
     },
     {
-        'id':4,
+        'id': 4,
         'name': 'Twitch Clone',
         'description': 'Twitch Clone is a streaming platform that allows users to watch and stream live videos. I developed this solution to provide a seamless streaming experience, allowing users to create, edit, and delete streams.Chat functionality is also available.',
-        'img':require('./twtich-clone.jpg'),
+        'img': require('./twtich-clone.jpg'),
         'link': 'https://twitch-clone-proc.vercel.app/'
     },
     {
         id: 5,
-        name:'Portfolio',
+        name: "Multiplayer Chess",
+        description: "Multiplayer Chess is a platform where users can play chess with their friends. I developed this solution to provide a seamless gaming experience, allowing users play chess with the random people around the world.",
+        img: require('./multiplayer-chess.png'),
+        link: "https://github.com/krish3957/multiplayer-chess"
+    },
+    {
+        id: 6,
+        name: "Bloggie",
+        description: "Bloggie is a platform where users can read and write blogs. I developed this solution to provide a seamless blogging experience, allowing users to read and write blogs. For proper formatting of the blog, I have used react-quill editor, which allows users to write blogs in a proper format.",
+        img: require('./blog.png'),
+        link: "https://github.com/krish3957/blog-client"
+    },
+    {
+        id: 7,
+        name: 'Portfolio',
         'description': 'My portfolio is a personal website that showcases my skills, projects, and experience. I developed this solution to provide a comprehensive overview of my work and experience.',
-        'img':require('./portfolio.jpg'),
+        'img': require('./portfolio.jpg'),
         'link': 'https://krish3957.github.io/portfolio/'
     }
 ]
